9fa45399548a53546545d1295c5605ed9c62b71d
stephen <snortey71@gmail.com>
2024-07-28 18:30:31 +0000

    model change

--- accounts/models.py
+++ accounts/models.py
@@ -34,6 +34,8 @@ class CustomUser(AbstractBaseUser):
         USER = 'user'
         SHOPOWNER = 'shop owner'
     email = models.EmailField(verbose_name='email address', max_length=255, unique=True)
+    first_name = models.CharField(max_length=255, unique=True, default=False)
+    last_name = models.CharField(max_length=255, unique=True, default=False)
     username = models.CharField(max_length=255, unique=True)
     role = models.CharField(max_length=20,choices=Roles.choices,default=Roles.USER,blank=True,null=True)
     is_active = models.BooleanField(default=True)
